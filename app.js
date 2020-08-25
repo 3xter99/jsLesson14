4febc0c3bb0b735e8a9a6903645f7b89f92a1fb8
3xter <3x-ter@mail.ru>
2020-08-25 14:44:42 +0300

    change lesson

--- app.js
+++ app.js
@@ -14,16 +14,21 @@ DomElement.prototype.createElem = function () {
         let element
     if (this.selector.split('')[0] === '.') {
         element = document.createElement('div')
-        element.classList.add(this.selector)
-    } else {
+        element.classList.add(this.selector.split('').slice(1).join(''))
+    } else if (this.selector.split('')[0] === '#'){
         element = document.createElement('p')
-        element.id = this.selector
+        element.id = this.selector.split('').slice(1).join('')
+    } else {
+        console.error('Не тот селектор')
     }
     element.textContent = 'Привет'
-    element.style.height = this.height
-    element.style.width = this.width
-    element.style.background = this.bg
-    element.style.fontSize = this.fontSize
+    element.style.cssText = `height: ${this.height};
+        width: ${this.width};
+        background: ${this.bg};
+        fontSize: ${this.fontSize};
+        position: absolute;
+        left: 50%;
+        top: 50%`
     body.append(element)
 }
 
