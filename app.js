310e0a569727e79a331cde18796d6acd6b66001e
3xter <3x-ter@mail.ru>
2020-09-02 17:08:08 +0300

    Did lesson18HARD

--- app.js
+++ app.js
@@ -1,37 +1,35 @@
+let startBtn = document.querySelector('.startBtn')
+let restBtn = document.querySelector('.restBtn')
+let boll = document.querySelector('.img')
+let count = 0;
+let flyInterval;
+let click = false
+let flyBoll = () => {
+    click = !click
+    let startFly = () => {
+        flyInterval = requestAnimationFrame(startFly)
+        count++
+        if (click) {
+            if (count < 400) {
+                boll.style.top = count + 'px'
 
-
-let body =document.querySelector('body')
-
-function DomElement(selector, height, width, bg, fontSize) {
-    this.selector = selector;
-    this.height = height;
-    this.width = width;
-    this.bg = bg;
-    this.fontSize = fontSize;
-}
-
-DomElement.prototype.createElem = function () {
-        let element
-    if (this.selector.split('')[0] === '.') {
-        element = document.createElement('div')
-        element.classList.add(this.selector.split('').slice(1).join(''))
-    } else if (this.selector.split('')[0] === '#'){
-        element = document.createElement('p')
-        element.id = this.selector.split('').slice(1).join('')
-    } else {
-        console.error('Не тот селектор')
+            } else {
+                cancelAnimationFrame(flyInterval)
+            }
+        } else {
+            cancelAnimationFrame(flyInterval)
+        }
     }
-    element.textContent = 'Привет'
-    element.style.cssText = `height: ${this.height};
-        width: ${this.width};
-        background: ${this.bg};
-        fontSize: ${this.fontSize};
-        position: absolute;
-        left: 50%;
-        top: 50%`
-    body.append(element)
-}
+    startFly()
 
-let newObj = new DomElement('#111', '100px', '100px', 'red', '20px')
-newObj.createElem()
 
+    }
+
+
+startBtn.addEventListener('click', flyBoll)
+
+restBtn.addEventListener('click', () => {
+    count = 0
+    boll.style.top = '0px'
+    cancelAnimationFrame(flyInterval)
+})
